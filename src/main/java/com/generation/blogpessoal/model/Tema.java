91a062ea9f152f4ca5f768697cc984b8c0bd6a96
Maria Luiza <maluduartetorres@gmail.com>
2025-05-20 14:52:07 -0300

    :sparkles: Implementação de Tema, TemaController, TemaRepository

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,47 @@
+package com.generation.blogpessoal.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo descrição é obrigatório!")
+    private String descricao;
+
+    @OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagens;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Postagem> getPostagens() {
+        return postagens;
+    }
+
+    public void setPostagens(List<Postagem> postagens) {
+        this.postagens = postagens;
+    }
+}
